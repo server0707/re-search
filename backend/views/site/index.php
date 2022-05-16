32d7fad92917e771faa73a5e4aecae71f5b7d2d3
Abdulla Abdullayev <abdullajon_uzb@mail.ru>
2022-05-16 19:05:25 +0500

    Fix

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -14,14 +14,14 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
             <h3 class="text-center">Aloqa</h3>
             <hr>
             <p class="text-center">
-                Sizda <?= ($newContactsCount > 0) ? "ta yangi aloqa habari mavjud." : "yangi aloqa habari mavjud emas." ?></p>
+                Sizda <?= ($newContactsCount > 0) ? $newContactsCount . "ta yangi aloqa habari mavjud." : "yangi aloqa habari mavjud emas." ?></p>
         </a>
         <a href="<?= \yii\helpers\Url::to(['/interactive-services']) ?>" class="col-lg-6 border border-dark"
            style="border-radius: 15px">
             <h3 class="text-center">Interaktiv xizmatlar</h3>
             <hr>
             <p class="text-center">
-                Sizda <?= ($newInteractiveServicesCount > 0) ? "ta yangi interaktiv xizmatlar habari mavjud." : "yangi interaktiv xizmatlar habari mavjud emas." ?></p>
+                Sizda <?= ($newInteractiveServicesCount > 0) ? $newInteractiveServicesCount . "ta yangi interaktiv xizmatlar habari mavjud." : "yangi interaktiv xizmatlar habari mavjud emas." ?></p>
         </a>
     </div>
 </div>
